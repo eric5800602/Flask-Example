d23c89430c4fc70491dd7cc7ad92bb01414b2d2d
SymbolWu <eric5800602@gmail.com>
2023-12-19 02:24:17 +0000

    Connect with mongo db and add send/query requests

--- modules/query.py
+++ modules/query.py
@@ -1,8 +1,27 @@
 from app import app
 from flask import request
+from pymongo import MongoClient
 
 
-@app.route('/q')
+client = MongoClient('mongodb.railway.internal', 27017)
+
+db = client.test
+todos = db.request
+
+@app.route('/send')
+def send():
+    try:
+        req = request.get_json()
+        user = req['user']
+        request_name = req['request_name']
+        post_id = todos.insert_one({user:request_name}).inserted_id
+        return "Got request " + post_id
+    except:
+        return "Request fail"
+
+
+@app.route('/query')
 def query():
-    args = request.args
+    args = todos.find_one()
+    todos.DeleteOne(args)
     return args
